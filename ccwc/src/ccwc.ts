add113f4f24ef419549b21ef01a8b425db701b36
Gangadhar184 <zenistuig65@gmail.com>
2025-06-11 00:46:08 +0530

    added linecount, wordcount, charactercount

--- ccwc/src/ccwc.ts
+++ ccwc/src/ccwc.ts
@@ -2,24 +2,41 @@ import * as fs from 'fs';
 
 const args: string[] = process.argv;
 
-if(args.length !== 4){
+if (args.length !== 4) {
     console.error("Usage: ccwc -c <filename>");
     process.exit(1);
 }
 
 const flag = args[2];
-if(flag !== '-c'){
-    console.error("unsupported flag -c");
+if (flag !== '-c' && flag !== '-l' && flag !== '-w' && flag !== '-m') {
+    console.error("unsupported flags -c, -l, -w, -m");
     process.exit(1);
 }
 
 const filename = args[3];
 
-try{
+try {
     const buffer = fs.readFileSync(filename);
-    const byteCount =  buffer.length;
-    console.log(byteCount);
-}catch(error){
+    if (flag === '-c') {
+        const byteCount = buffer.length;
+        console.log(byteCount);
+    }
+    else if (flag === '-l') {
+        const content = buffer.toString();
+        const lineCount = content.split('\n').length;
+        console.log(lineCount);
+    } else if (flag === '-w') {
+        const content = buffer.toString();
+        const words = content.trim().split(/\s+/);
+        const wordCount = words.length;
+        console.log(wordCount);
+    } else if (flag === '-m') {
+        const content = buffer.toString();
+        const charCount = content.length;
+        console.log(charCount);
+    }
+
+} catch (error) {
     console.error(`Error reading file "${filename}": ${(error as Error).message}`);
     process.exit(1);
 }
